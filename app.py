694e2a7cb9464b71bc74e4229304ec4edee8bf3e
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2022-10-06 09:54:36 +0100

    Start logging INFO level messages

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 from project import create_app
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = create_app()
