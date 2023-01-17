fcac1ecf5a10a65ac45c8c1cf7ca6036c5ae6b94
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2023-01-17 16:30:32 +0000

    add parameters argument to detector function

--- template/detector.py
+++ template/detector.py
@@ -1,7 +1,7 @@
 from scipy import stats
 
 #ksTest detector
-def detector(trainSet, liveSet):
+def detector(trainSet, liveSet, parameters):
     firstFeature = trainSet.axes[1][0]
     pValue = stats.ks_2samp(trainSet[firstFeature].to_list(), liveSet[firstFeature].to_list())[1]
     return int(pValue < 0.05), pValue
